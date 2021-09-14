cc629f450fff932698577032931137f2980feb30
Dovile T <do.zakarauskaite@gmail.com>
2021-09-14 19:29:26 +0300

    pridejau nd 1.1

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HelloWorld       //sia erdve pasivadinau taip ir pagal tai galesiu rasti
+{
+    class Program    // nereik pervardint sito pav
+    {
+        static void Main(string[] args)    // metodo (Main) pervardint jau negalim
+        {
+            Console.WriteLine("Koks tavo vardas?");
+            string name = Console.ReadLine();    // galima naudot vietoj string var visur kur string rasiau. jis nustatys koks tipas automatiskai skaitymo metu
+            
+            Console.WriteLine("Kokia tavo pavarde?");
+            string lastName = Console.ReadLine();
+           
+            Console.WriteLine("Kiek tau metu?");
+            string year = Console.ReadLine();
+           
+            Console.WriteLine(name + " " + lastName + " " + year); 
+            // arba rasau Console.WriteeLine(string.Concat(name , surname , age); CIA JAU REIKIA STRING RASYT, NEGALIMA VAR, NES CIA JAU TIPAS METODU
+            // arba rasau Console.WriteLine($"{name} {lastName} {year}");
+            // arba rasau Console.Write ("{0} {1} {2}", name, surname, age);
+            {
+
+
+            }
+        }
+    }
+}
---
+++ Homework1.1/Homework1.1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace Homework1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Įvesk apskritimo spindulį:");
+            string radiusInString = Console.ReadLine();
+            double radius = Convert.ToDouble(radiusInString, CultureInfo.InvariantCulture);
+
+            double c = 2 * Math.PI * radius;    // ilgis
+
+            double s = Math.PI * radius * radius;  // plotas
+
+            Console.WriteLine($"Apskrititimo ilgis yra: {c}");
+            Console.WriteLine($"Apskrititimo plotas yra: {s}");
+
+        }
+    }
+}
